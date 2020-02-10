00e377c3415d4b8ec64f7bf78012da1af5acd480
Clint Ki <ki2clint@gmail.com>
2020-02-10 13:27:21 -0600

    objects

---
+++ index.js
@@ -0,0 +1,18 @@
+const foods = {
+  hotdog: "bun",
+  Blt: "bacon lettuce tomato",
+  myFavorite: true,
+  greek: "gyro",
+  drinks: {
+    coffee: {
+      type: "medium roast"
+    }
+  }
+};
+
+const aKiExample = "aKi";
+const myObj = {
+  aKi: "example"
+};
+
+console.log(myObj[aKiExample]);
